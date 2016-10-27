e9d5534797e770bceb120a31dc96d584e1711460
Yuriy Taras <ytaras@softserveinc.com>
2016-10-27 14:00:38 +0300

    Actor-based generator

--- src/main/scala/generator/Stream.scala
+++ src/main/scala/generator/Stream.scala
@@ -3,33 +3,27 @@ package generator
 import java.util.UUID
 
 import akka.NotUsed
-import akka.actor.{ActorRef, ActorSystem, Cancellable, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.Producer
 import akka.stream._
 import akka.stream.scaladsl._
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import kamon.Kamon
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
-
 import scala.concurrent.duration._
 
 /**
   * Created by ytaras on 10/25/16.
   */
 object Stream  {
-  import StreamSyntax._
-  private val sampleIds = (1 to 50000).map(_ => UUID.randomUUID())
-
-  val initialLoadDeviceIds: Source[UUID, NotUsed] = Source(sampleIds).named("starting_device_ids")
+  private def sampleIds(implicit c: Config) = (1 to c.getInt("generator.num-devices")).map(_ => UUID.randomUUID())
 
   val collectDeviceIds: Flow[UUID, Set[UUID], NotUsed] =
     Flow[UUID]
       .map(x => Set(x))
       .reduce( _ ++_ )
-
-  val tick: Source[Unit, Cancellable] = Source.tick(0.second, 250.millis, ())
 
   def generateMeasures(distruptor: Measure => String): Flow[Set[UUID], String, NotUsed] =
     Flow[Set[UUID]]
@@ -37,20 +31,13 @@ object Stream  {
       .mapConcat(_.map(Measure.sample))
       .map(distruptor)
 
-  def tickedMeasuresActor(implicit as: ActorSystem) = Source
-      .actorRef[Measure](500000, OverflowStrategy.fail)
+  def tickedMeasuresActor(implicit as: ActorSystem, c: Config) = Source
+      .actorRef[Measure](c.getInt("generator.num-devices") * 100, OverflowStrategy.fail)
       .mapMaterializedValue(startDevices)
 
-  def tickedMeasuresStream = initialLoadDeviceIds.via(collectDeviceIds)
-      .wontFinish
-      .holdWithWait
-      .zip(tick)
-      .map(_._1)
-      .mapConcat(_.map(Measure.sample))
-
-  def startDevices(publishTo: ActorRef)(implicit as: ActorSystem) = {
+  def startDevices(publishTo: ActorRef)(implicit as: ActorSystem, c: Config) = {
     val deviceSupervisor = as.actorOf(
-      Props(classOf[DeviceActorSupervisor], 500.millis, publishTo),
+      Props(classOf[DeviceActorSupervisor], c.getDuration("generator.frequency").toMillis.millis, publishTo),
       "device_supervisor"
     )
     sampleIds.foreach { x => deviceSupervisor ! DeviceActorSupervisorApi.RegisterDevice(x) }
@@ -59,7 +46,7 @@ object Stream  {
   def main(args: Array[String]): Unit = {
     Kamon.start()
     val measuresCounter = Kamon.metrics.counter("measures-counter")
-    val config = ConfigFactory.load()
+    implicit val config = ConfigFactory.load()
     val disruptor = MessageDisruptor(config)
     val outputTo = config.getString("generator.topic.measures")
     implicit val as = ActorSystem()
